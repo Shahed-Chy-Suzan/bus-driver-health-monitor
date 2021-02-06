75f33258c86ebd232900d55798763c204059e977
ismailreformed <ismailplusbd1994@gmail.com>
2021-02-06 18:38:03 +0600

    created login and register page

---
+++ pages/registration/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container grid-list-lg class="pa-0">
+    <v-row no-gutters justify="center" align="center" :class="bp.mdAndUp ? 'mt-15' : ''">
+      <v-col
+        cols="12"
+        sm="12"
+        md="4"
+        lg="4"
+      >
+        <Registration />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Registration from '@/components/Auth/Registration'
+export default {
+  layout: 'basic',
+  name: 'Index',
+  components: {
+    Registration
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
